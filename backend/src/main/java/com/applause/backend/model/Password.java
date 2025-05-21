838e400766b5a26dbabfa8023a27e4d1466210c9
Thomas Borges <thomasborges_@hotmail.com>
2025-05-20 22:34:57 -0300

    adicionado userController

---
+++ backend/src/main/java/com/applause/backend/model/Password.java
@@ -0,0 +1,29 @@
+/*
+package com.applause.backend.model;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Password {
+
+    private String salt;
+    private String hashPassword;
+
+    public Password(){}
+
+    public Password(String salt, String hashPassword) {
+        this.salt = salt;
+        this.hashPassword = hashPassword;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getHashPassword() {return hashPassword;}
+
+    public void setHashPassword(String hashPassword) {
+        this.hashPassword = hashPassword;
+    }
+}
+*/
---
+++ backend/src/main/java/com/applause/backend/model/User.java
@@ -0,0 +1,22 @@
+/*package com.applause.backend.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @Embedded
+    private Password password;
+
+
+}*/
